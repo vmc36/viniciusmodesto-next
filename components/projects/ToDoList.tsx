64f414fc2fa86315e1dfbb46efce05dd11f2771d
Vinicius <vmcmodesto@gmail.com>
2023-10-31 14:27:50 -0300

    wip

--- components/projects/ToDoList.tsx
+++ components/projects/ToDoList.tsx
@@ -14,7 +14,7 @@ export default function ToDoList() {
   const { resolvedTheme } = useTheme();
 
   return (
-    <div className="h-[360px] bg-[#1A1A1A] rounded-xl relative overflow-hidden" ref={ref}>
+    <div className="h-[360px] bg-[#1A1A1A] rounded-xl relative overflow-hidden vmc:h-[250px]" ref={ref}>
       <Halo strength={resolvedTheme === 'light' ? 10 : 4}>
         <motion.div className="w-[257px] absolute right-4 z-10 -bottom-10" style={{ y }}></motion.div>
         <Image width={504} height={282} src="/projects/todolist/home.png" alt="todo list image" className="rounded-[26px]" />
