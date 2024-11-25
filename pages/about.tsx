4ad71ecee28e6bf06e29d0d43fa02da2c697f082
Vinicius Modesto <vmcmodesto@gmail.com>
2024-11-25 20:39:40 -0300

    fix:content site

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,122 +1,157 @@
-import Image from 'next/image';
-import { NextSeo } from 'next-seo';
+import Image from "next/image";
+import { NextSeo } from "next-seo";
 
-import Link from 'components/Link';
-import Section from 'components/Section';
-import Workplaces from 'components/Workplaces';
-import Gallery from 'components/Gallery';
+import Link from "components/Link";
+import Section from "components/Section";
+import Workplaces from "components/Workplaces";
+import Gallery from "components/Gallery";
 
-import siacomLogo from 'public/projects/siacom-logo.png';
-import tjprLogo from 'public/projects/tjpr-logo.png';
-import avatar from 'public/avatar.png';
+import siacomLogo from "public/companies/siacom-logo.png";
+import tjprLogo from "public/companies/tjpr-logo.png";
+import zenvialogo from "public/companies/zenvia.jpg";
+import test from "public/companies/test.jpg";
+
+import avatar from "public/avatar.png";
 
 export const connectLinks = [
-  { label: 'Email', href: 'mailto:viniciusmodesto.dev@gmail.com' },
-  { label: 'LinkedIn', href: 'https://www.linkedin.com/in/vinicius-modesto96/' },
-  { label: 'GitHub', href: 'https://github.com/vmc36' },
+ { label: "Email", href: "mailto:viniciusmodesto.dev@gmail.com" },
+ { label: "LinkedIn", href: "https://www.linkedin.com/in/vinicius-modesto96/" },
+ { label: "GitHub", href: "https://github.com/vmc36" },
 ];
 
 const workplaces = [
-  {
-    title: 'Frontend Developer',
-    description: 'Sia.Com',
-    time: '2021 - Now',
-    imageSrc: siacomLogo,
-    link: 'http://siacominformatica.com.br/',
-  },
-  {
-    title: 'Analista de Suporte',
-    description: 'Sia.Com',
-    time: '2020 - Now',
-    imageSrc: siacomLogo,
-    link: 'http://siacominformatica.com.br/',
-  },
-  {
-    title: 'Técnico em Informática',
-    description: 'Sia.Com',
-    time: '2019 - 2020',
-    imageSrc: siacomLogo,
-    link: 'http://siacominformatica.com.br/',
-  },
-  {
-    title: 'Estágio - Núcleo de informática',
-    description: 'Tribunal de Justiça do Estado do Paraná',
-    time: '2017 - 2018',
-    imageSrc: tjprLogo,
-    link: 'https://www.tjpr.jus.br/',
-  },
+ {
+  title: "Analista de Suporte N2 SaaS",
+  description: "Zenvia",
+  time: "2024 - Now",
+  imageSrc: test,
+  link: "https://www.zenvia.com",
+ },
+ {
+  title: "Frontend Developer",
+  description: "Sia.Com",
+  time: "2021 - Now",
+  imageSrc: siacomLogo,
+  link: "http://siacominformatica.com.br/",
+ },
+ {
+  title: "Analista de Suporte",
+  description: "Sia.Com",
+  time: "2020 - Now",
+  imageSrc: siacomLogo,
+  link: "http://siacominformatica.com.br/",
+ },
+ {
+  title: "Técnico em Informática",
+  description: "Sia.Com",
+  time: "2019 - 2020",
+  imageSrc: siacomLogo,
+  link: "http://siacominformatica.com.br/",
+ },
+ {
+  title: "Estágio - Núcleo de informática",
+  description: "Tribunal de Justiça do Estado do Paraná",
+  time: "2017 - 2018",
+  imageSrc: tjprLogo,
+  link: "https://www.tjpr.jus.br/",
+ },
 ];
 
-const seoTitle = 'Sobre mim | Vinicius Modesto';
-const seoDesc = 'Desenvolvedor Front-end, Analista de Suporte';
+const seoTitle = "Sobre mim | Vinicius Modesto";
+const seoDesc = "Desenvolvedor Front-end, Analista de Suporte";
 
 export default function About() {
-  return (
-    <>
-      <NextSeo
-        title={seoTitle}
-        description={seoDesc}
-        openGraph={{
-          title: seoTitle,
-          description: seoDesc,
-          url: `https://vinimodesto-dev.vercel.app/about/`,
-          site_name: 'Vinicius Modesto',
-        }}
-        twitter={{
-          cardType: 'summary_large_image',
-        }}
-      />
-      <div className="flex flex-col gap-16 md:gap-24">
-        <div className="hidden sm:block">
-          <Gallery />
-        </div>
-        <div className="-mb-8 sm:hidden animate-in">
-          <Image src={avatar} width={48} height={48} alt="avatar of Vinicius Modesto" />
-        </div>
-        <div className="flex flex-col gap-16 animate-in sm:animate-none md:gap-24" style={{ '--index': 2 } as React.CSSProperties}>
-          <Section heading="Sobre mim" headingAlignment="right">
-            <div className="flex flex-col gap-6">
-              <p>
-                <em className="font-semibold">Olá</em>&nbsp; Me chamo Vinícius, tenho 27 anos e trabalho com T.I há 9 e atualmente atuo com desenvolvimento
-                Front End e Suporte.
-              </p>
-              <p>
-                Comecei na carreira de T.I em 2014, ingressando na faculdade de Sistemas de Informação pela Universidade Paranaense - Unipar, em Umuarama - PR,
-                onde me formei 4 anos depois.
-              </p>
-              <p>Já atuei como técnico de informática, atualmente atuo como analista de suporte e desenvolvedor Front-end.</p>
-              <p>
-                Minha grande paixão é trabalhar com tecnologia, criando e entendendo como as coisas funcionam, compartilhando ideias e aprendendo com as pessoas
-                que tenho a oportunidade de conhecer na jornada. Possuo um background desde como funciona um computador a como gerenciar o armazenamento de
-                dados de uma aplicação em um navegador.
-              </p>
-              <p>
-                Fui criado no Paraná, mas atualmente moro em Recife-PE. Casado com a Aléxia, adoro viajar e como hobby gosto de jogar games como League of
-                Legends, fortnite ou assistir esportes. Meus times favoritos são: Arsenal (Premier League), Santos (Brasil), New England Patriots (NFL) e
-                Cleveland Cavaliers (NBA).
-              </p>
-            </div>
-          </Section>
-          <Section heading="Contato" headingAlignment="right">
-            <ul className="flex gap-6 animated-list">
-              {connectLinks.map((link) => (
-                <li className="transition-opacity" key={link.label}>
-                  <Link href={link.href}>{link.label}</Link>
-                </li>
-              ))}
-            </ul>
-          </Section>
-          <Section heading="Trabalho" headingAlignment="right">
-            <div className="flex flex-col w-full gap-8">
-              <p>
-                + de {new Date().getFullYear() - 2014} anos de experiência atuando com tecnologia, entre Hardware, atendimento ao cliente, suporte e
-                Desenvolvimento front-end.
-              </p>
-              <Workplaces items={workplaces} />
-            </div>
-          </Section>
-        </div>
+ return (
+  <>
+   <NextSeo
+    title={seoTitle}
+    description={seoDesc}
+    openGraph={{
+     title: seoTitle,
+     description: seoDesc,
+     url: `https://vinimodesto-dev.vercel.app/about/`,
+     site_name: "Vinicius Modesto",
+    }}
+    twitter={{
+     cardType: "summary_large_image",
+    }}
+   />
+   <div className="flex flex-col gap-16 md:gap-24">
+    <div className="hidden sm:block">
+     <Gallery />
+    </div>
+    <div className="-mb-8 sm:hidden animate-in">
+     <Image
+      src={avatar}
+      width={48}
+      height={48}
+      alt="avatar of Vinicius Modesto"
+     />
+    </div>
+    <div
+     className="flex flex-col gap-16 animate-in sm:animate-none md:gap-24"
+     style={{ "--index": 2 } as React.CSSProperties}
+    >
+     <Section heading="Sobre mim" headingAlignment="right">
+      <div className="flex flex-col gap-6">
+       <p>
+        Olá! Sou Vinícius Modesto, desenvolvedor Front-end com experiência
+        também em suporte técnico. Tenho {new Date().getFullYear() - 1996} anos,
+        moro em Recife - PE, e atuo com tecnologia há{" "}
+        {new Date().getFullYear() - 2014} anos, quando me formei na faculdade de
+        S.I.
+       </p>
+       <p>
+        Atualmente atuo na Zenvia como analista de suporte N2 de uma aplicação
+        SAAS amplamente utilizada.
+       </p>
+       <p>
+        Minha trajetória começou com suporte técnico e manutenção de
+        computadores, mas logo migrei para o universo de suporte e
+        desenvolvimento web.
+       </p>
+       <p>
+        Trabalhei por mais de 3 anos na Sia.Com, onde atuei como analista de
+        suporte e desenvolvedor. No suporte, atendia clientes, resolvia dúvidas
+        e bugs, realizava testes de software, treinava usuários e novos
+        colaboradores.
+       </p>
+
+       <p>
+        Já como desenvolvedor, participei da criação de páginas web usando
+        tecnologias como PHP,Laravel, VueJS e React, sempre buscando entregar
+        projetos bonitos e eficientes em parceria com designers e outros devs.
+        Meu foco está em criar soluções práticas e resolver problemas.
+       </p>
+
+       <p>
+        Além disso, valorizo muito o trabalho em equipe e a troca de ideias para
+        trazer o melhor resultado possível. Se quiser saber mais sobre mim, dá
+        uma olhada no meu LinkedIn ou no meu GitHub!
+       </p>
       </div>
-    </>
-  );
+     </Section>
+     <Section heading="Contato" headingAlignment="right">
+      <ul className="flex gap-6 animated-list">
+       {connectLinks.map((link) => (
+        <li className="transition-opacity" key={link.label}>
+         <Link href={link.href}>{link.label}</Link>
+        </li>
+       ))}
+      </ul>
+     </Section>
+     <Section heading="Trabalho" headingAlignment="right">
+      <div className="flex flex-col w-full gap-8">
+       <p>
+        + de {new Date().getFullYear() - 2014} anos de experiência atuando com
+        tecnologia, entre Hardware, atendimento ao cliente, suporte e
+        Desenvolvimento front-end.
+       </p>
+       <Workplaces items={workplaces} />
+      </div>
+     </Section>
+    </div>
+   </div>
+  </>
+ );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,140 +1,165 @@
-import { GetStaticProps } from 'next';
-import { allPosts, allProjects, Post, Project } from '.contentlayer/generated';
-import { pick } from '@contentlayer/client';
-import { Analytics } from '@vercel/analytics/react';
-import Section from 'components/Section';
+import { GetStaticProps } from "next";
+import { allPosts, allProjects, Post, Project } from ".contentlayer/generated";
+import { pick } from "@contentlayer/client";
+import { Analytics } from "@vercel/analytics/react";
+import Section from "components/Section";
 
-import NaTrave from 'components/projects/NaTrave';
-import ChaveNfe from 'components/projects/ChaveNfe';
-import BorderRadius from 'components/projects/BorderRadius';
-import CurrentWeather from 'components/projects/CurrentWeather';
-import ToDoList from 'components/projects/ToDoList';
+import NaTrave from "components/projects/NaTrave";
+import ChaveNfe from "components/projects/ChaveNfe";
+import BorderRadius from "components/projects/BorderRadius";
+import CurrentWeather from "components/projects/CurrentWeather";
+import ToDoList from "components/projects/ToDoList";
 
 type HomeProps = {
-  posts: Post[];
-  projects: Project[];
+ posts: Post[];
+ projects: Project[];
 };
 
 export default function Home({ projects }: HomeProps) {
-  return (
-    <>
-      <Analytics />
-      <div className="flex flex-col gap-20 md:gap-28">
-        <div>
-          <h1 className="animate-in">Vinicius Modesto</h1>
-          <p className="text-secondary animate-in" style={{ '--index': 1 } as React.CSSProperties}>
-            Front-end Developer
+ return (
+  <>
+   <Analytics />
+   <div className="flex flex-col gap-20 md:gap-28">
+    <div>
+     <h1 className="animate-in">Vinicius Modesto</h1>
+     <p
+      className="text-secondary animate-in"
+      style={{ "--index": 1 } as React.CSSProperties}
+     >
+      Front-end Developer e Analista de Suporte
+     </p>
+    </div>
+    <div
+     className="flex flex-col gap-4 animate-in"
+     style={{ "--index": 2 } as React.CSSProperties}
+    >
+     <h2>Projetos Selecionados</h2>
+     <ul className="flex flex-col gap-16">
+      <li>
+       Na Trave
+       <Section heading="2022">
+        <div className="flex flex-col gap-5">
+         <div className="flex flex-col gap-1">
+          <h3>Na Trave - Catar 2022</h3>
+          <p className="text-secondary">
+           {" "}
+           Esse projeto foi um bolão para a copa de 2022 no catar, onde havia
+           login, ranking e placares para as partidas. Foi um projeto full
+           stack, onde adorei demais o processo. Ferramentas utilizadas: react,
+           prisma, KoaJS, Planetscale, tailwind e vite.
           </p>
+          <a href="https://pickem-worldcup.vercel.app/" target="_blank">
+           <NaTrave />
+          </a>
+         </div>
         </div>
-        <div className="flex flex-col gap-4 animate-in" style={{ '--index': 2 } as React.CSSProperties}>
-          <h2>Projetos Selecionados</h2>
-          <ul className="flex flex-col gap-16">
-            <li>
-              Na Trave
-              <Section heading="2022">
-                <div className="flex flex-col gap-5">
-                  <div className="flex flex-col gap-1">
-                    <h3>Na Trave - Catar 2022</h3>
-                    <p className="text-secondary">
-                      {' '}
-                      Esse projeto foi um bolão para a copa de 2022 no catar, onde havia login, ranking e placares para as partidas. Foi um projeto full stack,
-                      onde adorei demais o processo. Ferramentas utilizadas: react, prisma, KoaJS, Planetscale, tailwind e vite.
-                    </p>
-                    <a href="https://pickem-worldcup.vercel.app/" target="_blank">
-                      <NaTrave />
-                    </a>
-                  </div>
-                </div>
-              </Section>
-            </li>
-            <li>
-              Consulta NFe
-              <Section heading="2023">
-                <div className="flex flex-col gap-5">
-                  <div className="flex flex-col gap-1">
-                    <p className="text-secondary">
-                      {' '}
-                      Desenvolvi essa aplicação que retira as informações a partir da chave da nota fiscal e mostra na tela, também fazendo uma consulta a
-                      empresa emissora via API. Esse foi um projeto onde eu como trabalhei como analista de suporte, pude unir a necessidade de conseguir uma
-                      informação fiscal de maneira mais fácil a partir da chave da nota.
-                    </p>
-                    <a href="https://chavenfe.vercel.app/" target="_blank">
-                      <ChaveNfe />
-                    </a>
-                  </div>
-                </div>
-              </Section>
-            </li>
-            <li>
-              Border radius generator
-              <Section heading="2022">
-                <div className="flex flex-col gap-5">
-                  <div className="flex flex-col gap-1">
-                    <p className="text-secondary">
-                      {' '}
-                      Foi um projeto simples, porém desenvolvido em comunidade junto a devsoutinholabs, onde o desafio foi lançado com o protótipo e as soluções
-                      foram enviadas no repositório com a hashtag #devsoutinhochallange. O processo foi bem bacana, simples do ponto de vista técnico porém
-                      interessante pela participação da comunidade.
-                    </p>
-                    <a href="https://border-radius-previewer-vmc36.vercel.app/" target="_blank">
-                      <BorderRadius />
-                    </a>
-                  </div>
-                </div>
-              </Section>
-            </li>
-            <li>
-              Current Weather
-              <Section heading="2022">
-                <div className="flex flex-col gap-5">
-                  <div className="flex flex-col gap-1">
-                    <p className="text-secondary">
-                      {' '}
-                      É uma aplicação que consome dados da API Climática (https://openweathermap.org/api) e exibe as informações baseadas na localização atual
-                      do usuário. Já existia a ideia de pegar a temperatura inserindo a cidade manualmente, então, fiz um upgrade utilizando o recurso de
-                      localização pelo browser e fazendo essa consulta de forma automática.
-                    </p>
-                    <a href="https://current-weather-api.vercel.app/" target="_blank">
-                      <CurrentWeather />
-                    </a>
-                  </div>
-                </div>
-              </Section>
-            </li>
-            <li>
-              To do List
-              <Section heading="2022">
-                <div className="flex flex-col gap-5">
-                  <div className="flex flex-col gap-1">
-                    <p className="text-secondary">
-                      {' '}
-                      Uma aplicação simples para atender uma necessidade pessoal de criar uma lista de coisas a se fazer de forma direta e prática. A sua task é
-                      criada e ao finalizar ela, pode se clicar para dizer que foi concluida ou exclui-la. Utiliza o localstorage do navegador para salvar as
-                      tarefas.
-                    </p>
-                    <a href="https://todolist-iota-sooty.vercel.app" target="_blank">
-                      <ToDoList />
-                    </a>
-                  </div>
-                </div>
-              </Section>
-            </li>
-          </ul>
+       </Section>
+      </li>
+      <li>
+       Consulta NFe
+       <Section heading="2023">
+        <div className="flex flex-col gap-5">
+         <div className="flex flex-col gap-1">
+          <p className="text-secondary">
+           {" "}
+           Desenvolvi essa aplicação que retira as informações a partir da chave
+           da nota fiscal e mostra na tela, também fazendo uma consulta a
+           empresa emissora via API. Esse foi um projeto onde eu como trabalhei
+           como analista de suporte, pude unir a necessidade de conseguir uma
+           informação fiscal de maneira mais fácil a partir da chave da nota.
+          </p>
+          <a href="https://chavenfe.vercel.app/" target="_blank">
+           <ChaveNfe />
+          </a>
+         </div>
         </div>
-      </div>
-    </>
-  );
+       </Section>
+      </li>
+      <li>
+       Border Radius Generator
+       <Section heading="2022">
+        <div className="flex flex-col gap-5">
+         <div className="flex flex-col gap-1">
+          <p className="text-secondary">
+           {" "}
+           Foi um projeto simples, porém desenvolvido em comunidade junto a
+           devsoutinholabs, onde o desafio foi lançado com o protótipo e as
+           soluções foram enviadas no repositório com a hashtag
+           #devsoutinhochallange. O processo foi bem bacana, simples do ponto de
+           vista técnico porém interessante pela participação da comunidade.
+          </p>
+          <a
+           href="https://border-radius-previewer-vmc36.vercel.app/"
+           target="_blank"
+          >
+           <BorderRadius />
+          </a>
+         </div>
+        </div>
+       </Section>
+      </li>
+      <li>
+       Current Weather
+       <Section heading="2022">
+        <div className="flex flex-col gap-5">
+         <div className="flex flex-col gap-1">
+          <p className="text-secondary">
+           {" "}
+           É uma aplicação que consome dados da API Climática
+           (https://openweathermap.org/api) e exibe as informações baseadas na
+           localização atual do usuário. Já existia a ideia de pegar a
+           temperatura inserindo a cidade manualmente, então, fiz um upgrade
+           utilizando o recurso de localização pelo browser e fazendo essa
+           consulta de forma automática.
+          </p>
+          <a href="https://current-weather-api.vercel.app/" target="_blank">
+           <CurrentWeather />
+          </a>
+         </div>
+        </div>
+       </Section>
+      </li>
+      <li>
+       To do List
+       <Section heading="2022">
+        <div className="flex flex-col gap-5">
+         <div className="flex flex-col gap-1">
+          <p className="text-secondary">
+           {" "}
+           Uma aplicação simples para atender uma necessidade pessoal de criar
+           uma lista de coisas a se fazer de forma direta e prática. A sua task
+           é criada e ao finalizar ela, pode se clicar para dizer que foi
+           concluida ou exclui-la. Utiliza o localstorage do navegador para
+           salvar as tarefas.
+          </p>
+          <a href="https://todolist-iota-sooty.vercel.app" target="_blank">
+           <ToDoList />
+          </a>
+         </div>
+        </div>
+       </Section>
+      </li>
+     </ul>
+    </div>
+   </div>
+  </>
+ );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = allPosts
-    .sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime())
-    .filter((_, i) => i < 4)
-    .map((post) => pick(post, ['slug', 'title', 'publishedAt', 'image']));
+ const posts = allPosts
+  .sort(
+   (a, b) =>
+    new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
+  )
+  .filter((_, i) => i < 4)
+  .map((post) => pick(post, ["slug", "title", "publishedAt", "image"]));
 
-  const projects = allProjects.map((post) => pick(post, ['slug', 'title', 'description', 'time']));
+ const projects = allProjects.map((post) =>
+  pick(post, ["slug", "title", "description", "time"])
+ );
 
-  return {
-    props: { posts, projects },
-  };
+ return {
+  props: { posts, projects },
+ };
 };
